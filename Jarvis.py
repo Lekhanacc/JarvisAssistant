22bbd41ebe79a6e48d0e28ebafffa66239ecfd55
Lekhana.C <clekhana224@gmail.com>
2025-12-10 19:59:21 +0530

    change in functioning.modified to both voice and and text input

--- Jarvis.py
+++ Jarvis.py
@@ -1,111 +1,104 @@
-import pyttsx3 #pip install pyttsx3
-import speech_recognition as sr #pip install speechRecognition
+import pyttsx3
+import speech_recognition as sr
 import datetime
-import wikipedia #pip install wikipedia
+import wikipedia
 import webbrowser
 import os
-import smtplib
-
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-# print(voices[1].id)
-engine.setProperty('voice', voices[0].id)
-
 
 def speak(audio):
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    engine.setProperty('rate', 170)
     engine.say(audio)
     engine.runAndWait()
 
-
-def wishMe():
-    hour = int(datetime.datetime.now().hour)
-    if hour>=0 and hour<12:
-        speak("Good Morning!")
-
-    elif hour>=12 and hour<18:
-        speak("Good Afternoon!")   
-
-    else:
-        speak("Good Evening!")  
-
-    speak("I am Jarvis Sir. Please tell me how may I help you")       
-
-def takeCommand():
-    #It takes microphone input from the user and returns string output
-
+def takeVoiceCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Listening...")
+        print("Listening (voice)...")
         r.pause_threshold = 1
         audio = r.listen(source)
 
     try:
-        print("Recognizing...")    
+        print("Recognizing...")
         query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}\n")
+        print(f"Voice: {query}")
+        return query
+    except:
+        print("Could not understand voice. Try typing instead.")
+        return None
 
-    except Exception as e:
-        # print(e)    
-        print("Say that again please...")  
-        return "None"
-    return query
+def takeInput():
+    print("\nSpeak OR Type your command:")
+    text = input("Type here (or press Enter to use voice): ").strip()
 
-def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('youremail@gmail.com', 'your-password')
-    server.sendmail('youremail@gmail.com', to, content)
-    server.close()
+    if text != "":
+        print(f"Text: {text}")
+        return text.lower()
+    else:
+        return takeVoiceCommand()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour < 12:
+        speak("Good Morning!")
+    elif hour < 18:
+        speak("Good Afternoon!")
+    else:
+        speak("Good Evening!")
+
+    speak("I am Jarvis. You can talk to me or type commands.")
 
 if __name__ == "__main__":
     wishMe()
-    while True:
-    # if 1:
-        query = takeCommand().lower()
 
-        # Logic for executing tasks based on query
+    while True:
+        query = takeInput()
+
+        if query is None:
+            continue
+        query = query.lower()
+
         if 'wikipedia' in query:
-            speak('Searching Wikipedia...')
+            speak("Searching Wikipedia")
             query = query.replace("wikipedia", "")
             results = wikipedia.summary(query, sentences=2)
-            speak("According to Wikipedia")
             print(results)
             speak(results)
 
         elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
+            speak("Opening YouTube")
+            webbrowser.open("https://youtube.com")
 
         elif 'open google' in query:
-            webbrowser.open("google.com")
+            speak("Opening Google")
+            webbrowser.open("https://google.com")
 
         elif 'open stackoverflow' in query:
-            webbrowser.open("stackoverflow.com")   
+            speak("Opening StackOverflow")
+            webbrowser.open("https://stackoverflow.com")
 
+        elif 'play music' in query or 'open music' in query:
+            speak("Opening YouTube Music")
+            webbrowser.open("https://music.youtube.com/")
 
-        elif 'play music' in query:
-            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
-            songs = os.listdir(music_dir)
-            print(songs)    
-            os.startfile(os.path.join(music_dir, songs[0]))
-
-        elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-            speak(f"Sir, the time is {strTime}")
+        elif 'time' in query:
+            now = datetime.datetime.now().strftime("%I:%M %p")
+            print(f"The current time is {now}")
+            speak(f"The current time is {now}")
 
         elif 'open code' in query:
-            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            codePath = "C:\\Users\\Lekana\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
             os.startfile(codePath)
+            speak("Opening Visual Studio Code")
 
-        elif 'email to harry' in query:
-            try:
-                speak("What should I say?")
-                content = takeCommand()
-                to = "harryyourEmail@gmail.com"    
-                sendEmail(to, content)
-                speak("Email has been sent!")
-            except Exception as e:
-                print(e)
-                speak("Sorry my friend harry bhai. I am not able to send this email")    
+        elif 'stop' in query or 'exit' in query or 'quit' in query or 'bye' in query:
+            speak("Thank you for using me. Goodbye!")
+            print("Jarvis stopped.")
+            break
+
         else:
-            print("No query matched")
+            print("No command matched.")
+
+   
